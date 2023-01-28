b7010cbf7c613b1e499784284dde0f50264e80df
John Kim <96427720+userkimjh@users.noreply.github.com>
2023-01-28 20:34:30 +0800

    added weight dashboard layout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
+import React, {useState} from 'react';
 import './App.css';
 import Sidebar from './components/Sidebar';
+import Chart from './components/Chart';
+import Dashboard from './components/Dashboard';
+
+import {Box} from "@mui/material";
+import WeightDisplay from './components/WeightDisplay';
 
 function App() {
+
+  const drawerWidth = 240;
+  const [showPage, SetShowPage] = useState("WEIGHT")
+
   return (
-    <Sidebar
-      name={"Admin Test"}
-    />
+    <Box sx={{ display: 'flex' }}>
+      <Sidebar 
+        name={"John Kim"}
+        drawerWidth={drawerWidth}
+      />
+      {showPage === "DASHBOARD" &&
+        <Dashboard
+        drawerWidth={drawerWidth}
+        />
+      }
+      {/* {showPage === "Food" &&
+        <FoodDisplay/>
+      } */}
+      {showPage === "WEIGHT" &&
+        <WeightDisplay/>
+      }
+    </Box>
   );
 }
 
---
+++ src/components/WeightDisplay.jsx
@@ -0,0 +1,96 @@
+import React, {useState, useRef} from "react";
+import { TextField, InputAdornment, Box, Toolbar, Button } from "@mui/material";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+
+import { Line, getDatasetAtEvent } from 'react-chartjs-2'
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+export default function WeightDisplay() {
+  const [weightInput, setWeightInput] = useState("")
+  const [weightData, setWeightData] = useState([3,4,5])
+  const [weightLabel, setWeightLabel] = useState(['Jun', 'Jul', 'Aug'])
+  const chartRef = useRef();
+
+
+  const addData = (data) => {
+    setWeightLabel(prev => [...prev, "Oct"])
+    setWeightData(prev =>[...prev, data]) 
+    // chart = chart.current;
+    // chart.data.labels.push(label);
+    // chart.data.datasets.forEach((dataset) => {
+    //   dataset.data.push(Math.floor(Math.random() * 100));
+    // });
+    // // chart.data.datasets[0].data.push(Math.floor(Math.random() * 100));
+
+    // chart.update();
+
+    // setWeightInput("")
+    // console.log(chart.data.datasets[0])
+  }
+  return (
+    <Box
+      component="main"
+    >
+      <Toolbar />
+      <TextField
+        label="Weight"
+        id="outlined-start-adornment"
+        sx={{ m: 1, width: '25ch' }}
+        InputProps={{
+          startAdornment: <InputAdornment position="start">kg</InputAdornment>,
+        }}
+        value={weightInput}
+        onChange={e => {
+          e.preventDefault()
+          setWeightInput(e.target.value)
+          }
+        }
+      />
+      <Button
+        type="submit"
+        variant="contained" 
+        onClick={(e) => {
+          e.preventDefault();
+          if (Number(weightInput)){
+            addData(Number(weightInput))
+          };
+        }
+      }
+      >
+        Add Weight
+      </Button>
+      <Line
+        ref={chartRef}
+        datasetIdKey='id'
+        data={{
+          labels: weightLabel,
+          datasets: [
+            {
+              id: 1,
+              label: '',
+              data: weightData,
+            }
+          ],
+        }}
+      />
+    </Box>
+  )
+}
