c55c5c1427ab4e703fffa4e02a8f9cf80bbb4b1a
John Kim <96427720+userkimjh@users.noreply.github.com>
2023-03-01 21:13:05 +0800

    moved api fetching to backend to hide api key and fixed api fetching issue from client to server where response was not coming back

--- server/index.js
+++ server/index.js
@@ -1,12 +1,22 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+require('dotenv').config()
 
 app.use(cors());
 app.use(express.json());
 
-app.get("/message", (req, res) => {
-  res.json({ message: "Hello from server!" });
+const API_KEY = process.env.NODE_ENV;
+
+app.get("/api_key", async (req, res) => {
+  const query = req.query.food
+  fetch(`https://api.nal.usda.gov/fdc/v1/foods/search?api_key=${API_KEY}&query=${query}&dataType=Branded`)
+    .then(res =>{
+      console.log("fetching data nodejs")
+      if (res.ok) {return res.json();}
+      throw new Error('Something went wrong');
+    })
+    .then(data => res.send(data))
 });
 
 app.listen(8000, () => {
