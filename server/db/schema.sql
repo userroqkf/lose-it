cc6bfd3933692f0321f631536f680c952bbe5299
John Kim <96427720+userkimjh@users.noreply.github.com>
2023-03-23 21:22:10 +0800

    started login using auth0

--- server/db/schema.sql
+++ server/db/schema.sql
@@ -10,8 +10,7 @@ CREATE TABLE users (
   id SERIAL PRIMARY KEY,
   first_name VARCHAR(255) NOT NULL,
   last_name VARCHAR(255) NOT NULL,
-  email VARCHAR(255) NOT NULL,
-  password VARCHAR(255) NOT NULL
+  user_token VARCHAR(255) NOT NULL,
 );
 CREATE TABLE weights (
   id SERIAL PRIMARY KEY,
